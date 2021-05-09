d30b1cf8f7a78db52c0401257975c0072f5d9b45
CxrlosKenobi <yolitopinto@gmail.com>
2021-05-09 18:23:20 -0400

    Offline prototype running!

--- db/gen.py
+++ db/gen.py
@@ -11,29 +11,11 @@ from api import get_current_time
 conn = sqlite3.connect('data.db')
 cur = conn.cursor()
 
-#   Asign random values to test
-bmp_280 = 0
-bmp_280_pr = 0
-
-hdc1080 = 0
-hdc1080_hu = 0
-
-neo6m = 0
-neo6m_la = 0
-neo6m_lo = 0
-neo6m_al = 0
-
-mpu9250_ac = 0
-mpu9250_gy = 0
-mpu9250_ma = 0
-
-now = datetime.now()
-
 #   Create packet prototype
-prot_pckt = f'{now.strftime('%d/%m, %H:%M:%S')};{get_current_time()};{bmp280_te},{bmp280_pr};{hdc1080_te},{hdc1080_hu};{neo6m_la},{neo6m_lo},{neo6m_al};{mpu9250_ac};{mpu9250_gy};{mpu9250_ma}'
+# prot_pckt = f'{now.strftime('%d/%m, %H:%M:%S')};{get_current_time()};{bmp280_te},{bmp280_pr};{hdc1080_te},{hdc1080_hu};{neo6m_la},{neo6m_lo},{neo6m_al};{mpu9250_ac};{mpu9250_gy};{mpu9250_ma}'
 
 #   Example of packet's format
-packet = '08-05,14:57:21;15512;38.66,950.60;36.8,29.1;-36.121,-71.814,190.985;-0.00179,0.007725,1.001726;-0.00179,0.007725,1.001726;-0.00179,0.007725,1.001726'
+# packet = '08-05,14:57:21;15512;38.66,950.60;36.8,29.1;-36.121,-71.814,190.985;-0.00179,0.007725,1.001726;-0.00179,0.007725,1.001726;-0.00179,0.007725,1.001726'
 # 08-05,14:57:21 ; 15512 ; 38.66,950.60 ; 36.8,29.1 ; -36.121,-71.814,190.985 ; -0.00179,0.007725,1.001726 ; -0.00179,0.007725,1.001726 ; -0.00179, 0.007725,1.001726
 
 #   Decoding
@@ -60,11 +42,28 @@ packet = '08-05,14:57:21;15512;38.66,950.60;36.8,29.1;-36.121,-71.814,190.985;-0
 for i in range(1, 700):
     print(f'[ {i} ] Running ...')
     now = datetime.now()
+    #   Asign random values to test
+    timestamp = now.strftime('%d/%m, %H:%M:%S')
+    timeinsec = get_current_time()
+    bmp_280 = 0
+    bmp280_pr = 0
+
+    hdc1080 = 0
+    hdc1080_hu = 0
+
+    neo6m = 0
+    neo6m_la = 0
+    neo6m_lo = 0
+    neo6m_al = 0
+
+    mpu9250_ac = 0
+    mpu9250_gy = 0
+    mpu9250_ma = 0
+
+    bmp280_te = random.randint(1, 100)
+    hdc1080_te = random.randint(1, 100)
     
-    bmp_280_te = random.randint(5, 30)
-    hdc1080_te = random.randint(1,20)
-    
-    packet = f'{now.strftime('%d/%m, %H:%M:%S')};{get_current_time()};{bmp280_te},{bmp280_pr};{hdc1080_te},{hdc1080_hu};{neo6m_la},{neo6m_lo},{neo6m_al};{mpu9250_ac};{mpu9250_gy};{mpu9250_ma}'
+    packet = f"""{now.strftime('%d/%m, %H:%M:%S')};{get_current_time()};{bmp280_te},{bmp280_pr};{hdc1080_te},{hdc1080_hu};{neo6m_la},{neo6m_lo},{neo6m_al};{mpu9250_ac};{mpu9250_gy};{mpu9250_ma}"""
 
     cur.execute('''
         INSERT INTO data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
